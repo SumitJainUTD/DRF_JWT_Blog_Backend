a81c4280a12ba9032835a0e4c3d9fd45c8db51e9
SumitJainUTD <sumi12june@gmail.com>
2021-08-27 23:29:21 -0500

    code refactor

---
+++ blog/app/post/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.utils import timezone
+from ..users.models import CustomUser as User
+
+
+class Post(models.Model):
+    id = models.CharField(max_length=80, primary_key=True)
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    slug = models.SlugField(blank=True, unique=True)
---
+++ blog/app/post/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .serializers import PostSerializer
+from .models import Post
+from django.http import JsonResponse
+# Create your views here.
+
+
+@api_view(['GET', ])
+def api_post_view(request, slug):
+    try:
+        post = Post.objects.get(slug=slug)
+        print(post)
+    except Post.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        print("here 3")
+        serializer = PostSerializer(post)
+        print(serializer.data)
+        return JsonResponse(serializer.data)
+        # return Response(serializer.data)
