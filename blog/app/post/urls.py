7f55103e831e9d3e31886a3478020110c3f495e0
SumitJainUTD <sumi12june@gmail.com>
2021-09-02 23:13:13 -0500

    health end point added

--- blog/app/post/urls.py
+++ blog/app/post/urls.py
@@ -3,12 +3,15 @@ from .views import (
     api_get_posts_view,
     api_create_posts_view,
     ApiBlogListView,
-    api_update_blog_view
+    api_update_blog_view,
+    api_get_health
+
 )
 
 app_name = 'post'
 
 urlpatterns = [
+    path('health/', api_get_health, name='health'),
     path('<slug>/', api_get_posts_view, name='detail'),
     path('<slug>/update', api_update_blog_view, name="update"),
     path('create', api_create_posts_view, name='create_post'),
